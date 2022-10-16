84e427c3a8f4683e08335577d18ebcdfeef03a8e
Omar AbdElwahhab <omarabdelwahhabkishk2000@gmail.com>
2022-10-17 01:55:17 +0200

    edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,78 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href=" {{ asset('css/home.css') }}">
-    <link rel="stylesheet" href=" {{ asset('css/all.min.css') }}">
-    <style>
-        .sign-out{
-            background-color: inherit;
-            color: white;
-            border-collapse: collapse;
-            margin-left: 5px;
-        }
-        .sign-out:hover{
-            padding: 8px;
-            background-color: rgb(64, 149, 205);
-        }
-        .upload{
-            background-color: rgb(10, 44, 95);
-            border-radius:10px;
-            padding:10px; 
-            color: white;
-        }
-        .upload:hover{
-            background-color: rgb(64, 149, 205); 
-        }
-    </style>
-    <title>Home</title>
-</head>
-
-<body>
-
-
-    <header>
-        <img src="{{ asset('images/medical.jpg') }}" alt="">
-        <ul>
-            <li class="header-li"><a class="home" href="{{ route('home') }}">Home</a></li>
-            
-            <li class="header-li">
-                <form style="display: inline" method="post" action="{{ route('signout') }}">
-                    @csrf
-                    <button class="sign-out">Sign Out</button>
-                </form>
-                
-            </li>
-        </ul>
-    </header>
-
-    <div class="home-div">
-        <form action="{{route("image.upload")}}" method="post" enctype="multipart/form-data">
-            @csrf
-            <input type="file" name="image">
-            @error('image')
-                <div class="alert alert-danger text-center m-4">
-                    {{ $message }}
-                </div>
-            @enderror
-            <div class="text-center">
-                <button type="submit" class="upload">Upload</button>
+@extends("includes.app")
+@section('title', 'Home')
+@section("content")
+<div class="home-div">
+    <form action="{{route("image.upload")}}" method="post" enctype="multipart/form-data">
+        @csrf
+        <input type="file" name="image">
+        @error('image')
+            <div class="alert alert-danger text-center m-4">
+                {{ $message }}
             </div>
-        </form>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-        crossorigin="anonymous"></script>
-
-    <script src="{{ asset('js/all.min.js') }}"></script>
-
-
-</body>
-
-</html>
+        @enderror
+        <div class="text-center">
+            <button type="submit" class="upload">Upload</button>
+        </div>
+    </form>
+</div>
+@endsection
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,42 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
-
-    <title>Sign Up</title>
-
-</head>
-<body>
+@extends("includes.auth.app")
+@section('title', 'Sign-In')
+@section("content")
     <div class="sign-form">
         <div class="logo">
-             <img src="{{asset("/images/medical.jpg")}}" alt="">
+            <img src="{{asset("/images/medical.jpg")}}" alt="">
         </div>
         <div class="register">
             <form  action="{{route("signup")}}" method="post" >
                 @csrf
                 <input class="form-control" type="text" name="name" placeholder="Enter Name">
                 @error('name')
-                    <div class="alert alert-danger text-center m-4">
-                        {{ $message }}
-                    </div>
+                <div class="alert alert-danger text-center m-4">
+                    {{ $message }}
+                </div>
                 @enderror
                 <input type="email" name="email" placeholder="Enter Email">
                 @error('email')
-                    <div class="alert alert-danger text-center m-4">
-                        {{ $message }}
-                    </div>
+                <div class="alert alert-danger text-center m-4">
+                    {{ $message }}
+                </div>
                 @enderror
                 <input type="password" name="password" placeholder="Enter Password">
                 @error('password')
-                    <div class="alert alert-danger text-center m-4">
-                        {{ $message }}
-                    </div>
+                <div class="alert alert-danger text-center m-4">
+                    {{ $message }}
+                </div>
                 @enderror
                 <div class="text-center">
                     <button class="sign" type="submit">Sign Up</button>
@@ -47,13 +35,4 @@
         </div>
 
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-    crossorigin="anonymous"></script>
-
-    <script src="{{ asset('js/all.min.js') }}"></script>
-
-
-</body>
-</html>
+@endsection
